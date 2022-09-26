a04e6fd37cd2707457fa09db4e8b859f62c7d57f
Aman Negi <lokeshnegi649@gmail.com>
2022-09-26 13:03:51 +0530

    host changed

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+export const localhost = "http://localhost:5000";
+export const host = "https://chit-chat-mern-app.onrender.com";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
