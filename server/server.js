c8ab4852f3d824e2ca17b56582849d49ef7982fd
Aman Negi <lokeshnegi649@gmail.com>
2022-09-26 12:28:29 +0530

    production changes

--- server/server.js
+++ server/server.js
@@ -23,18 +23,18 @@ app.use("/api/messages", messageRoutes);
 
 // -----------Deployment----------------
 
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/client/build")));
+// const __dirname1 = path.resolve();
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running successfully");
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname1, "client", "build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running successfully");
+//   });
+// }
 
 //-----------Deployment-----------------
 
